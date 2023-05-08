6c18084bd59b15d47e746e26f40cb2991ea40e35
nat_soloviova <natsolovyova2@gmail.com>
2023-05-08 14:46:19 +0300

    add task

--- Main.java
+++ Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         BankApplication bankApplication = new BankApplication();
-        System.out.println("1");
         try {
             bankApplication.process("accountId000", 50, "USD");
         } catch (WrongAccountException e) {
@@ -20,7 +19,6 @@ public class Main {
         } finally {
             System.out.println("Дякуємо, що скористалися нашим сервісом");
         }
-        System.out.println("2");
         try {
             bankApplication.process("accountId003", 250, "HRV");
         } catch (WrongAccountException e) {
@@ -33,7 +31,6 @@ public class Main {
             System.out.println("Сталася помилка при процесінгу, спробуйте ще раз");
         } finally {
             System.out.println("Дякуємо, що скористалися нашим сервісом");
-            System.out.println("3");
             try {
                 bankApplication.process("accountId001", 50, "EUR");
             } catch (WrongAccountException e) {
@@ -46,7 +43,6 @@ public class Main {
                 System.out.println("Сталася помилка при процесінгу, спробуйте ще раз");
             } finally {
                 System.out.println("Дякуємо, що скористалися нашим сервісом");
-                System.out.println("4");
                 try {
                     bankApplication.process("accountId001", 50, "USD");
                 } catch (WrongAccountException e) {
@@ -59,7 +55,6 @@ public class Main {
                     System.out.println("Сталася помилка при процесінгу, спробуйте ще раз");
                 } finally {
                     System.out.println("Дякуємо, що скористалися нашим сервісом");
-                    System.out.println("4");
                     try {
                         bankApplication.process("accountId001", 50, "USD");
                     } catch (WrongAccountException e) {
